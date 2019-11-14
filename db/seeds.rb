c9d84b84d39db5a94eead7fa9546b816fc62d700
claire froelich <claire.froelich@gmail.com>
2019-11-14 11:56:46 -0500

    added ingredients seed from cocktail db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+###########################
+# SEED INGREDIENTS
+###########################
+puts "deleting old ingredients..."
+Ingredient.destroy_all
+
+puts "creating new ingredients..."
+
+# get ingredients json
+doc = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+# get ingredients array from cocktail db
+ingredients = JSON.parse(doc)["drinks"].map! { |i| i["strIngredient1"]}
+
+ingredients.each do |i|
+  Ingredient.create({ name: i })
+end
+
+puts "done!"
